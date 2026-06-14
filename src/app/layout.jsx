c4e7eb52d13981050b94875147874a02dcd6a87a
Rohit Gora <rohit@example.com>
2026-06-14 16:11:35 +0530

    analytics: integrate Google Tag Manager (GTM) into root layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -57,6 +57,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MHPC543M');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
         {/* Google Analytics (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-N7B0CBR4R6"
@@ -115,6 +126,16 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="font-sans antialiased text-gray-900 bg-white">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MHPC543M"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <a href="#main-content" className="skip-to-content">Skip to main content</a>
         <main id="main-content">
           {children}
